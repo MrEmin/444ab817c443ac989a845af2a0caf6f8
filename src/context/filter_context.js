5638fd0610c6f1807b170b304ce96f204bfc3378
MrEmin <emuhammet.ozdemir@gmail.com>
2024-01-16 22:40:42 +0300

    context structure added

---
+++ src/context/filter_context.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/filter_reducer";
+import {
+  LOAD_PRODUCTS,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from "../actions";
+import { useProductsContext } from "./products_context";
+
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  sort: "price-lowest",
+  filters: {
+    text: "",
+    category: "all",
+    company: "all",
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+  },
+};
+const FilterContext = React.createContext();
+
+export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
+  useEffect(() => {
+    dispatch({ type: FILTER_PRODUCTS });
+    dispatch({ type: SORT_PRODUCTS });
+  }, [products, state.sort, state.filters]);
+
+  const updateSort = (e) => {
+    const value = e.target.value;
+    dispatch({ type: UPDATE_SORT, payload: value });
+  };
+
+  const updateFilters = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    if (name === "category") {
+      value = e.target.textContent;
+    }
+    if (name === "color") {
+      value = e.target.dataset.color;
+    }
+
+    if (name === "price") {
+      value = Number(value);
+    }
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
+  };
+  const clearFilters = () => {
+    dispatch({ type: CLEAR_FILTERS });
+  };
+
+  return (
+    <FilterContext.Provider
+      value={{
+        ...state,
+        updateSort,
+        updateFilters,
+        clearFilters,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+};
+// make sure use
+export const useFilterContext = () => {
+  return useContext(FilterContext);
+};
